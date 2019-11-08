69cca0a2423b67f88ef58f3f77032775bcf852a4
Mads W <madswandersen@hotmail.com>
2019-11-08 13:26:37 +0100

    Rettet sign up og login
    Nu har forms samme stil som de tilsvarende sider

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const imggo
+
+
+
+function validation(){
+var name = document.getElementById('email').value;
+var number = document.getElementById('number').value;
+var password = document.getElementById('password').value;
+if(name=='')
+{
+  alert('required');
+  return false;
+}
