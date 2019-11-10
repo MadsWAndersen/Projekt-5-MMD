dd0881df2672a3aa3a2ceaccde703ed503694a32
SigneMH <54638106+SigneMH@users.noreply.github.com>
2019-11-10 21:37:31 +0100

    profile update12
    tried some js -- can't input bf click edit

--- profilejava.js
+++ profilejava.js
@@ -1,6 +1,26 @@
 //cant use input forms//
 
+	
+function newFunction(){
+	
+let test = document.getElementsByClassName("label");
+	test.classList.remove("label");
+}
+
+document.getElementById("inp1").readOnly=true;
+
+
+
+
+
 //click 'edit button' to edit input forms//
+
+
+//function myFunction() {
+//  document.getElementById("inp1").contentEditable = true;
+//  document.getElementById("save").innerHTML = "The p element above is now editable. Try to change its text.";
+//}
+
 
 //on click change 'edit button' to 'save/submit' button//
 
@@ -8,4 +28,3 @@
 
 //on click 'save/submit' button, save changes//
 
-//function saveEdit() {
