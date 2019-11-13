7417dee699647dee50bc075af9296b3f94d6573c
Rasmus Frost <ucl@rasmusfrost.dk>
2019-11-13 12:48:07 +0100

    Main js fixed
    -Cleanup in js
    - Fixed z-index for nav-bar
    - deleted unused code
    - Fixed inproper positioning for js

--- mainjs.js
+++ mainjs.js
@@ -2,16 +2,14 @@ let left = document.getElementById("leftcard");
 let mid = document.getElementById("midcard");
 let right = document.getElementById("rightcard");
 
-// var editBtn = document.getElementById("edit-btn");
 
 
 function leftSort() {
   function leftCheck() {
     document.getElementById("input1").checked = true;
   }
-leftCheck();
 
-
+  leftCheck();
   right.classList.add("kort1");
   if (right.classList.contains("kort1")) {
     right.classList.remove("kort2", "kort3");
@@ -26,14 +24,16 @@ leftCheck();
   if (mid.classList.contains("kort3")) {
     mid.classList.remove("kort1", "kort2");
   }
-style.cssText ="-webkit-transition: all 0.5s ease-in-out;";
+  left.cssText = "-webkit-transition: all 0.5s ease-in-out;";
 }
+
+
 
 function midSort() {
   function midCheck() {
     document.getElementById("input2").checked = true;
   }
-midCheck();
+  midCheck();
   left.classList.add("kort1");
   if (left.classList.contains("kort1")) {
     left.classList.remove("kort2", "kort3");
@@ -49,9 +49,7 @@ midCheck();
     right.classList.remove("kort1", "kort2");
   }
 
-style.cssText ="-webkit-transition: all 0.5s ease-in-out;";
-
-
+  mid.cssText = "-webkit-transition: all 0.5s ease-in-out;";
 }
 
 
@@ -59,8 +57,8 @@ function rightSort() {
   function rightCheck() {
     document.getElementById("input3").checked = true;
   }
-rightCheck();
 
+  rightCheck();
   mid.classList.add("kort1");
   if (mid.classList.contains("kort1")) {
     mid.classList.remove("kort2", "kort3");
@@ -74,55 +72,29 @@ rightCheck();
   left.classList.add("kort3");
   if (left.classList.contains("kort3")) {
     left.classList.remove("kort1", "kort2");
-
   }
 
-
-style.cssText ="-webkit-transition: all 0.5s ease-in-out;";
-
-
+  right.cssText = "-webkit-transition: all 0.5s ease-in-out;";
 }
+
 left.addEventListener('click', leftSort);
 mid.addEventListener('click', midSort);
 right.addEventListener('click', rightSort);
-// function midSort() {
-//
-//     mid.classList.add("kort2");
-//   if(mid.classList.contains("kort2")){
-//     mid.classList.remove("kort3");
-//
-//   }
-//   left.classList.add("kort2");
-//   if (left.classList.contains("kort2")) {
-//     left.classList.remove("kort1");
-//
-//
-//
-// }
-// right.classList.add("kort1");
-// if (right.classList.contains("kort1")) {
-// right.classList.remove("kort3");
-// }
-// }
-//
-// window.alert();
 
 
-// let leftCard
+
 //
-// function cardSort() {
-// kort1.classList.add("active");
-// 		if (hejsa.classList.contains("active")) {
-// 		hejsa.readOnly=false;
+// let arrayEksempel = ["en", "to", "tre", "fire"];
 //
-// hejsa2.classList.add("active");
-// 		if (hejsa2.classList.contains("active")) {
-// 		hejsa2.readOnly=false;
+// for(let i = 0; i < arrayEksempel.length; i++){
 //
-// hejsa3.classList.add("active");
-// 		if (hejsa3.classList.contains("active")) {
-// 		hejsa3.readOnly=false;
-// 	}
-// 	}
-// 	}
+//    console.log(arrayEksempel[i]);
+//
 // }
+
+/* Outputs:
+one
+two
+three
+four
+*/
